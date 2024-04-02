7cb04dffcdb5000176caed5ee06086c0249ec4c3
Trivo-AmrMohammed <165791008+Trivo-AmrMohammed@users.noreply.github.com>
2024-04-02 15:36:27 +0300

    sapphire

--- src/main/java/net/trivo/tutorialmodtrivo/TutorialModTrivo.java
+++ src/main/java/net/trivo/tutorialmodtrivo/TutorialModTrivo.java
@@ -1,6 +1,7 @@
 package net.trivo.tutorialmodtrivo;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,28 +12,25 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.trivo.tutorialmodtrivo.item.ModItems;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(tutorialmodtrivo.MOD_ID)
-public class tutorialmodtrivo
+@Mod(TutorialModTrivo.MOD_ID)
+public class TutorialModTrivo
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmodtrivo";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public tutorialmodtrivo()
+    public TutorialModTrivo()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
     }
@@ -42,20 +40,20 @@ public class tutorialmodtrivo
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
 
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
---
+++ src/main/java/net/trivo/tutorialmodtrivo/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.trivo.tutorialmodtrivo.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.trivo.tutorialmodtrivo.TutorialModTrivo;
+
+public class ModItems {
+    public  static  final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialModTrivo.MOD_ID);
+
+    public static  final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public  static  void  register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
